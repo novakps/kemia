0376627c1519c2099285fd4dd1fe081959858cf6
Mark Rynbeek <markr@localhost.localdomain>
2010-10-28 14:10:37 +0100

    up-or-down now renders correctly and can be stretched as well

--- kemia/view/single_up_or_down_bond_renderer.js
+++ kemia/view/single_up_or_down_bond_renderer.js
@@ -1,5 +1,5 @@
 /** 
- * Copyright 2010 Paul Novak (paul@wingu.com)
+ * Copyright 2010 Mark Rijnbeek (markr@ebi.ac.uk)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,9 +17,13 @@
 goog.provide('kemia.view.SingleUpOrDownBondRenderer');
 goog.require('kemia.view.BondRenderer');
 
+goog.require('goog.debug');
+goog.require('goog.debug.FancyWindow');
+goog.require('goog.debug.Logger');
+
 /**
- * Class to render an single up or down bond object to a graphics representation
- * 
+ * Class to render an single up or down bond object to a graphics representation,
+ * a zig zag line.
  *
  * @param {goog.graphics.AbstractGraphics} graphics to draw on.
  * @param {Object=} opt_config override default configuration
@@ -35,44 +39,51 @@ kemia.view.SingleUpOrDownBondRenderer = function(graphics, opt_config) {
 goog.inherits(kemia.view.SingleUpOrDownBondRenderer,
 		kemia.view.BondRenderer);
 
-kemia.view.SingleUpOrDownBondRenderer.prototype.render = function(bond,
-		transform, path) {
-	
+
+kemia.view.SingleUpOrDownBondRenderer.prototype.render = function(bond,transform, bondPath) {
+
 	this.setTransform(transform);
+    var zigZags = Math.round(goog.math.Coordinate.distance(bond.source.coord, bond.target.coord)*12);
+	if (zigZags<8)
+	   zigZags=8
 
-	var width = this.config.get("bond")['stroke']['width'] / 10;
-	var theta = kemia.view.BondRenderer.getTheta(bond);
+    this.logger.fine('# zigZags '+zigZags);
 
-	var angle_left = theta + (Math.PI / 2);
-	var angle_right = theta - (Math.PI / 2);
+    var strokeWidth = this.config.get("bond")['stroke']['width']/10 ;
+    var theta = kemia.view.BondRenderer.getTheta(bond);
+    var angle_left = theta + (Math.PI / 2);
+    var angle_right = theta - (Math.PI / 2);
 
-	var transleft = new kemia.graphics.AffineTransform(1, 0, 0, 1, Math
-			.cos(angle_left)
-			* width, Math.sin(angle_left) * width);
+    zzCoords = new Array();
+	zzCoords.push(bond.source.coord);
+    this.logger.fine('# coords '+zzCoords.length);
 
-	var transright = new kemia.graphics.AffineTransform(1, 0, 0, 1, Math
-			.cos(angle_right)
-			* width, Math.sin(angle_right) * width);
+    var trans=null;
+	var inc=1/zigZags;
+	var corr=0;
 
-	var leftside = transleft.transformCoords( [bond.source.coord, bond.target.coord]);
-	var rightside = transright.transformCoords([bond.source.coord, bond.target.coord]);
+    //Loop one: create the points to zig zag to, increasingly away from the middle line by using "corr"
+    for(z=1; z <= zigZags; z++) {
+        corr += inc;
 
-	var coords = transform.transformCoords( [ leftside[0],
-			leftside[1], rightside[0], rightside[1] ]);
+		if (z%2==0)
+            trans = new kemia.graphics.AffineTransform(1, 0, 0, 1, Math.cos(angle_left)* (strokeWidth*corr), Math.sin(angle_left) * (strokeWidth*corr));
+		else
+            trans = new kemia.graphics.AffineTransform(1, 0, 0, 1, Math.cos(angle_right) * (strokeWidth*corr), Math.sin(angle_right) * (strokeWidth*corr));
+        
+        var zX = ( ((zigZags-z)*bond.source.coord.x) + (bond.target.coord.x * z ))/zigZags; 
+        var zY = ( ((zigZags-z)*bond.source.coord.y) + (bond.target.coord.y * z ))/zigZags; 
 
-	var stroke = new goog.graphics.Stroke(
-			this.config.get("bond")['stroke']['width'], this.config.get("bond")['stroke']['color']);
-	var fill = null;
-
-	path.moveTo(coords[0].x, coords[0].y);
-	for ( var j = 1, lines = 10; j < lines; j++) {
-		if (j % 2) {
-			path.lineTo(coords[0].x + (coords[1].x - coords[0].x) * j / lines,
-					coords[0].y + (coords[1].y - coords[0].y) * j / lines);
-		} else {
-			path.lineTo(coords[2].x + (coords[3].x - coords[2].x) * j / lines,
-					coords[2].y + (coords[3].y - coords[2].y) * j / lines);
-		}
+		var intermediateTarget = trans.transformCoords( [new goog.math.Coordinate(zX,zY) ])[0];    
+        zzCoords.push(intermediateTarget);
+        
 	}
+    var coords = transform.transformCoords( zzCoords);
 
+    //Loop two: draw lines between zig zag points    
+    bondPath.moveTo(coords[1].x, coords[1].y);
+	for (i=2, max=coords.length-1; i<max; i++) {
+        bondPath.lineTo(coords[i].x, coords[i].y);
+        bondPath.moveTo(coords[i].x, coords[i].y);
+	}
 }
