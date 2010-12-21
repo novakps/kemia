4936ad4c1652237d37e1c3def3d113666d95be6b
Paul Novak <novakps@gmail.com>
2010-12-21 14:04:05 -0500

    refactor all_tests to use plovr

---
+++ kemia/all_tests_plovr.js
@@ -0,0 +1,9 @@
+{
+	"id":"all_tests",
+	"paths":".",
+	"inputs":["all_tests_runner.js", "all_tests.js"],
+	"level": "VERBOSE",
+	"mode" : "ADVANCED",
+	"debug": true,
+	"export-test-functions": true
+}
--- kemia/io/smiles/smiles_parser_test_plovr.js
+++ kemia/io/smiles/smiles_parser_test_plovr.js
@@ -1,7 +1,7 @@
 {
-	"id":"smiles",
+	"id":"io_smiles_parser_test",
 	"paths":[".", "../../model", "../../ring", "../../resource", "../../graphics", "../../math"],
-	"inputs":"plovr-inputs.js",
+	"inputs":"smiles_parser_test.js",
 	"level": "VERBOSE",
 	"mode" : "ADVANCED",
 	"debug": true,
