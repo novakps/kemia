af9f491e013b7912b58fdbfd0898319ad904fbfb
Paul Novak <novakps@gmail.com>
2011-01-04 09:48:36 -0500

    configure ADVANCED compilation for demo

--- kemia/controller/reaction_editor_demo_plovr.js
+++ kemia/controller/reaction_editor_demo_plovr.js
@@ -4,8 +4,9 @@
 	"inputs":"reaction_editor_demo.js",
 	"level": "VERBOSE",
 	"mode" : "ADVANCED",
-	"debug": true,
-	"pretty-print" : true,
-	"print-input-delimiter": true,
-	"export-test-functions": true
+	"define": {
+		  "goog.DEBUG": false
+		},
+	"debug": false,
+	"output-wrapper" : "/*\n * Kemia JavaScript Library %REVISION%\n * http://http://kemia.github.com/\n * \n * Copyright (c) 2010 Paul Novak (paul@wingu.com)\n * \n * Licensed under the Apache License, Version 2.0 \n * http://www.apache.org/licenses/LICENSE-2.0\n * \n * Date: %TIMESTAMP%\n * Revision: %REVISION%\n */\n(function(){%output%})();"
 }
